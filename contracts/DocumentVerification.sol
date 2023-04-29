90ba0c8a55b2b567e1d5032aad876c92b3259e1d
Vincent Dela Cruz <83751632+VincentjamesDC@users.noreply.github.com>
2023-04-29 11:04:29 +0800

    commit

--- contracts/DocumentVerification.sol
+++ contracts/DocumentVerification.sol
@@ -1,70 +1,26 @@
-// SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
 
-library MyipfsHash {
-    function getHash(string memory _hash) public pure returns (bytes32) {
-        bytes memory hashBytes = bytes(_hash);
-        uint256 hashLength = hashBytes.length;
-        bytes32 hash;
-
-        assembly {
-            hash := mload(add(_hash, 32))
-        }
-
-        return hash;
-    }
-}
+//import "https://github.com/ethereum/solidity-examples/tree/master/src/ipfs";
 
 contract DocumentVerification {
-    MyipfsHash public ipfsHash; // declare IPFS hash library variable
-
     struct Document {
-        address owner;
-        bytes32 documentHash;
         string ipfsHash;
-        uint timestamp;
         bool verified;
+        address owner;
     }
-
+    
     mapping(bytes32 => Document) public documents;
-
-    event DocumentAdded(address owner, bytes32 documentHash, string ipfsHash, uint timestamp);
-    event DocumentVerified(bytes32 documentHash);
-
-    constructor(address _ipfsHash) {
-        ipfsHash = MyipfsHash(_ipfsHash); // initialize IPFS hash library
+    
+    function addDocument(bytes32 hash, string memory ipfsHash) public {
+        require(documents[hash].owner == address(0), "Document already exists");
+        
+        documents[hash] = Document(ipfsHash, false, msg.sender);
     }
-
-    function addDocument(bytes32 _documentHash, string memory _ipfsHash) public {
-        require(documents[_documentHash].owner == address(0), "Document already exists");
-        require(bytes(_ipfsHash).length > 0, "Invalid IPFS hash");
-
-        documents[_documentHash] = Document({
-            owner: msg.sender,
-            documentHash: _documentHash,
-            ipfsHash: _ipfsHash,
-            timestamp: block.timestamp,
-            verified: false
-        });
-
-        emit DocumentAdded(msg.sender, _documentHash, _ipfsHash, block.timestamp);
-    }
-
-    function verifyDocument(bytes32 _documentHash) public {
-        require(documents[_documentHash].owner == msg.sender, "You don't own this document");
-        require(!documents[_documentHash].verified, "Document already verified");
-
-        documents[_documentHash].verified = true;
-
-        emit DocumentVerified(_documentHash);
-    }
-
-    function getDocument(bytes32 _documentHash) public view returns (address, string memory, uint, bool) {
-        Document storage document = documents[_documentHash];
-        return (document.owner, document.ipfsHash, document.timestamp, document.verified);
-    }
-
-    function getIPFSHash(string memory _hash) public view returns (bytes32) {
-        return ipfsHash.getHash(_hash);
+    
+    function verifyDocument(bytes32 hash) public {
+        require(documents[hash].owner != address(0), "Document does not exist");
+        require(msg.sender == documents[hash].owner, "Only document owner can verify");
+        
+        documents[hash].verified = true;
     }
 }
--- src/App.js
+++ src/App.js
@@ -1,94 +1,55 @@
 import React, { useState } from "react";
-import { ethers } from "ethers";
-import DocumentVerification from "./contracts/DocumentVerification.json";
+import Web3 from "web3";
+import { create } from "ipfs-http-client";
 
-function App() {
-  // eslint-disable-next-line no-unused-vars
-  const [provider, setProvider] = useState(null);
-  const [contract, setContract] = useState(null);
-  const [documentHash, setDocumentHash] = useState("");
-  const [ipfsHash, setIPFSHash] = useState("");
-  const [status, setStatus] = useState("");
+const ipfs = create("https://ipfs.infura.io:5001/api/v0");
 
-  const connectWallet = async () => {
-    if (window.ethereum) {
-      try {
-        // eslint-disable-next-line no-unused-vars
-        const accounts = await window.ethereum.request({
-          method: "eth_requestAccounts",
-        });
-        const provider = new ethers.providers.Web3Provider
-        (window.ethereum);
-        const signer = provider.getSigner();
-        const network = await provider.getNetwork();
-        const contractAddress = DocumentVerification.networks[network.chainId].address;
-        const contract = new ethers.Contract(contractAddress, DocumentVerification.abi, signer);
-        setProvider(provider);
-        setContract(contract);
-      } catch (error) {
-        console.error(error);
-      }
-    } else {
-      alert("Please install MetaMask to use this application.");
-    }
-  };
+const DocumentVerification = ({ contractAddress, abi }) => {
+  const [file, setFile] = useState();
+  const [hash, setHash] = useState();
+  const [status, setStatus] = useState();
+
+  const web3 = new Web3(Web3.givenProvider);
+  const contract = new web3.eth.Contract(abi, contractAddress);
 
   const addDocument = async () => {
-    try {
-      const ipfsHashBytes32 = await contract.getIPFSHash(ipfsHash);
-      await contract.addDocument(documentHash, ethers.utils.parseBytes32String(ipfsHashBytes32));
-      setStatus("Document added to the blockchain.");
-    } catch (error) {
-      console.error(error);
-      setStatus("Error adding document to the blockchain.");
-    }
+    const buffer = await file.arrayBuffer();
+    const result = await ipfs.add(buffer);
+    const ipfsHash = result.path;
+
+    const accounts = await web3.eth.getAccounts();
+    const hash = await contract.methods
+      .addDocument(web3.utils.soliditySha3(ipfsHash))
+      .send({ from: accounts[0] });
+
+    setHash(hash);
   };
 
   const verifyDocument = async () => {
-    try {
-      await contract.verifyDocument(documentHash);
-      setStatus("Document verified on the blockchain.");
-    } catch (error) {
-      console.error(error);
-      setStatus("Error verifying document on the blockchain.");
-    }
-  };
+    const accounts = await web3.eth.getAccounts();
+    const hash = await contract.methods
+      .verifyDocument(web3.utils.soliditySha3(hash))
+      .send({ from: accounts[0] });
 
-  const getDocument = async () => {
-    try {
-      const document = await contract.getDocument(documentHash);
-      setStatus("");
-      alert(
-        `Owner: ${document[0]}\nIPFS Hash: ${document[1]}\nTimestamp: ${new Date(
-          document[2] * 1000
-        ).toLocaleString()}\nVerified: ${document[3]}`
-      );
-    } catch (error) {
-      console.error(error);
-      setStatus("Error getting document from the blockchain.");
-    }
+    setStatus("Verified");
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Blockchain-based Document Verification with IPFS</h1>
-        <button onClick={connectWallet}>Connect Wallet</button>
+    <div>
+      <h1>Document Verification</h1>
+      <input type="file" onChange={(e) => setFile(e.target.files[0])} />
+      <button onClick={addDocument}>Add Document</button>
+      {hash && (
         <div>
-          <label>Document Hash:</label>
-          <input type="text" value={documentHash} onChange={(e) => setDocumentHash(e.target.value)} />
+          <p>Document hash: {hash.transactionHash}</p>
+          {!status && (
+            <button onClick={verifyDocument}>Verify Document</button>
+          )}
+          {status && <p>{status}</p>}
         </div>
-        <div>
-          <label>IPFS Hash:</label>
-          <input type="text" value={ipfsHash} onChange={(e) => setIPFSHash(e.target.value)} />
-        </div>
-        <button onClick={addDocument}>Add Document</button>
-        <button onClick={verifyDocument}>Verify Document</button>
-        <button onClick={getDocument}>Get Document</button>
-        <div>{status}</div>
-      </header>
+      )}
     </div>
   );
-}
+};
 
-export default App;
+export default DocumentVerification;
